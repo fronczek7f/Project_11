71384667f9dfe4ae1b51d10737a9bb9cc5d94255
fronczek7 <fronczek7f@gmail.com>
2018-12-21 23:47:43 +0100

    Ostatnia poprawka

--- app/src/main/java/com/android/fronc/project_11/MainActivity.java
+++ app/src/main/java/com/android/fronc/project_11/MainActivity.java
@@ -1,17 +1,29 @@
 package com.android.fronc.project_11;
 
+import android.app.Activity;
 import android.content.ContentValues;
 import android.net.Uri;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Handler handler = new Handler();
     private EditText editTextID;
     private EditText editTextContent;
+    private ListView listViewContents;
+    private CustomArrayAdapter customArrayAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +32,10 @@ public class MainActivity extends AppCompatActivity {
 
         editTextID = (EditText) findViewById(R.id.et_id);
         editTextContent = (EditText) findViewById(R.id.et_content);
+        listViewContents = (ListView) findViewById(R.id.lv_contents);
+        customArrayAdapter = new CustomArrayAdapter(this,  SimpleContentProvider.dataCollector);
+        listViewContents = (ListView) findViewById(R.id.lv_contents);
+        listViewContents.setAdapter(customArrayAdapter);
     }
 
     public void onClickAdd(View view) {
@@ -29,28 +45,93 @@ public class MainActivity extends AppCompatActivity {
         Uri uri = getContentResolver().insert(SimpleContentProvider.CONTENT_URI, values);
 
         Toast.makeText(getBaseContext(), uri.toString(), Toast.LENGTH_LONG).show();
+        customArrayAdapter.notifyDataSetChanged();
     }
 
     public void onClickDelete(View view) {
-        if (editTextID.getText().toString().trim() == "") {
-            getContentResolver().delete(SimpleContentProvider.CONTENT_URI, null, null);
+        Uri uriUpdate = Uri.withAppendedPath(SimpleContentProvider.CONTENT_URI, editTextID.getText().toString());
+        String id = editTextID.getText().toString().trim();
+        if (id.equals("")) {
+            final int count = getContentResolver().delete(SimpleContentProvider.CONTENT_URI, null, null);
             Toast.makeText(getBaseContext(), SimpleContentProvider.CONTENT_URI.toString(), Toast.LENGTH_LONG).show();
+            handler.postDelayed(new Runnable() {
+                public void run() {
+                    Toast.makeText(getBaseContext(), count + " lines has been deleted", Toast.LENGTH_LONG).show();
+                }
+            }, 4000);
         } else {
-            int id = getContentResolver().delete(Uri.withAppendedPath(SimpleContentProvider.CONTENT_URI, editTextID.getText().toString()), editTextID.getText().toString(), null);
+            getContentResolver().delete(uriUpdate, id, null);
             Toast.makeText(getBaseContext(), SimpleContentProvider.CONTENT_URI.toString() + "/" + id, Toast.LENGTH_LONG).show();
+            handler.postDelayed(new Runnable() {
+                public void run() {
+                    Toast.makeText(getBaseContext(), "1 lines has been deleted", Toast.LENGTH_LONG).show();
+                }
+            }, 4000);
         }
+        customArrayAdapter.notifyDataSetChanged();
     }
 
     public void onClickUpdate(View view) {
         ContentValues values = new ContentValues();
+        Uri uriUpdate;
+        String id = editTextID.getText().toString().trim();
         values.put(SimpleContentProvider.CONTENT, editTextContent.getText().toString());
 
-        if (editTextID.getText().toString().trim() == "") {
-            getContentResolver().update(SimpleContentProvider.CONTENT_URI, values, null, null);
+        if (id.equals("")) {
+            final int count = getContentResolver().update(SimpleContentProvider.CONTENT_URI, values, null, null);
             Toast.makeText(getBaseContext(), SimpleContentProvider.CONTENT_URI.toString(), Toast.LENGTH_LONG).show();
+            handler.postDelayed(new Runnable() {
+                public void run() {
+                    Toast.makeText(getBaseContext(), count + " lines has been updated", Toast.LENGTH_LONG).show();
+                }
+            }, 4000);
         } else {
-            int id = getContentResolver().update(Uri.withAppendedPath(SimpleContentProvider.CONTENT_URI, editTextID.getText().toString()), values, editTextID.getText().toString(), null);
+            uriUpdate = Uri.withAppendedPath(SimpleContentProvider.CONTENT_URI, id);
+            getContentResolver().update(uriUpdate, values, id, null);
             Toast.makeText(getBaseContext(), SimpleContentProvider.CONTENT_URI.toString() + "/" + id, Toast.LENGTH_LONG).show();
+            handler.postDelayed(new Runnable() {
+                public void run() {
+                    Toast.makeText(getBaseContext(), "1 lines has been updated", Toast.LENGTH_LONG).show();
+                }
+            }, 4000);
         }
+        customArrayAdapter.notifyDataSetChanged();
+    }
+}
+
+class CustomArrayAdapter extends ArrayAdapter<String> {
+
+    private Activity activity;
+    private List<String> list;
+
+    public CustomArrayAdapter(Activity activity, List<String> list) {
+        super(activity, R.layout.listlayoutforcontentprovider, list);
+        this.activity = activity;
+        this.list = list;
+    }
+
+    static class ViewHolder {
+        public TextView tvId;
+        public TextView tvContent;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        ViewHolder viewHolder;
+        View view = convertView;
+        if (view == null) {
+            LayoutInflater layoutInflater = activity.getLayoutInflater();
+            view = layoutInflater.inflate(R.layout.listlayoutforcontentprovider, null, true);
+            viewHolder = new ViewHolder();
+            viewHolder.tvId = (TextView) view.findViewById(R.id.tv_id);
+            viewHolder.tvContent = (TextView) view.findViewById(R.id.tv_content);
+            view.setTag(viewHolder);
+        } else {
+            viewHolder = (ViewHolder) view.getTag();
+        }
+
+        viewHolder.tvId.setText(String.valueOf(position));
+        viewHolder.tvContent.setText(list.get(position));
+        return view;
     }
 }
--- app/src/main/java/com/android/fronc/project_11/SimpleContentProvider.java
+++ app/src/main/java/com/android/fronc/project_11/SimpleContentProvider.java
@@ -9,7 +9,8 @@ import android.net.Uri;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 
-import java.util.Vector;
+import java.util.ArrayList;
+import java.util.List;
 
 public class SimpleContentProvider extends ContentProvider {
 
@@ -17,20 +18,19 @@ public class SimpleContentProvider extends ContentProvider {
     static final String URL = "content://" + PROVIDER_NAME + "/lines";
     static final Uri CONTENT_URI = Uri.parse(URL);
 
-    static final String ID = "id";
     static final String CONTENT = "content";
 
     static final int CONTENTS = 1;
     static final int CONTENT_ID = 2;
 
     static final UriMatcher uriMatcher;
-    static{
+    static {
         uriMatcher = new UriMatcher(UriMatcher.NO_MATCH);
         uriMatcher.addURI(PROVIDER_NAME, "lines", CONTENTS);
         uriMatcher.addURI(PROVIDER_NAME, "lines/#", CONTENT_ID);
     }
 
-    static Vector<String> dataCollector = new Vector<>();
+    static List<String> dataCollector = new ArrayList<>();
 
     @Override
     public boolean onCreate() {
@@ -46,10 +46,6 @@ public class SimpleContentProvider extends ContentProvider {
     @Nullable
     @Override
     public String getType(@NonNull Uri uri) {
-//        switch (uriMatcher.match(uri)) {
-//            case CONTENTS:
-//                return PROVIDER_NAME + "";
-//        }
         return null;
     }
 
@@ -65,7 +61,6 @@ public class SimpleContentProvider extends ContentProvider {
         dcId = dataCollector.indexOf(dcContent);
         _uri = ContentUris.withAppendedId(CONTENT_URI, dcId);
         getContext().getContentResolver().notifyChange(_uri, null);
-        System.out.println(dataCollector);
 
         return _uri;
     }
@@ -79,16 +74,15 @@ public class SimpleContentProvider extends ContentProvider {
 
         switch (uriMatcher.match(uri)) {
             case CONTENTS:
+                deleted = dataCollector.size();
                 dataCollector.clear();
-                System.out.println(dataCollector);
                 break;
 
             case CONTENT_ID:
                 dcId = Integer.valueOf(selection);
                 _uri = ContentUris.withAppendedId(CONTENT_URI, dcId);
-                dataCollector.add(Integer.valueOf(selection), "<null>");
+                dataCollector.set(Integer.valueOf(selection), "<null>");
                 getContext().getContentResolver().notifyChange(_uri, null);
-                deleted = Integer.valueOf(selection);
                 System.out.println(dataCollector);
                 break;
 
@@ -110,20 +104,18 @@ public class SimpleContentProvider extends ContentProvider {
         switch (uriMatcher.match(uri)) {
             case CONTENTS:
                 dcContent = values.get(CONTENT).toString();
-                for (int i = 0; i < dataCollector.size(); i++) {
+                update = dataCollector.size();
+                for (int i = 0; i < update; i++) {
                     dataCollector.set(i, dcContent);
                 }
-                System.out.println(dataCollector);
                 break;
 
             case CONTENT_ID:
                 dcContent = values.get(CONTENT).toString();
                 dcId = Integer.valueOf(selection);
                 _uri = ContentUris.withAppendedId(CONTENT_URI, dcId);
-                dataCollector.add(Integer.valueOf(selection), dcContent);
+                dataCollector.set(Integer.valueOf(selection), dcContent);
                 getContext().getContentResolver().notifyChange(_uri, null);
-                update = Integer.valueOf(selection);
-                System.out.println(dataCollector);
                 break;
 
             default:
